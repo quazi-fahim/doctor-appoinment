4646ac96225b48b5b6313f14d86da6dc4cf8f9a6
Fahim <fahimahmad.4388@gmail.com>
2025-02-20 12:35:27 +0530

    This commit will add the following:-  - Basic node.js app setup.  - Database connection using mongoose.  - Basic controllers/models/rputes.

---
+++ controllers/homeController.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports.home=function(req,res){
+  res.send("Hello from basenode app.");
+}
---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URL;
+
+const mongoDB = async () => {
+  await mongoose.connect(url)
+    .then(async () => {
+        console.log('Connected to the database ')
+    }).catch ( (err) => {
+  console.error(`Error connecting to the database. n${err}`);
+})
+}
+module.exports = mongoDB;
